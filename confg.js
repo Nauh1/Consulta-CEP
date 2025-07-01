c9be8012030184f1af3352ab4e9d9d3ffbd97e6a
NauH1 <souzavinicius840@gmail.com>
2025-07-01 09:04:07 -0300

    commit

--- confg.js
+++ confg.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const cep = document.getElementById("cep");
+    const nome = document.getElementById("nome");
+    const salvar = document.getElementById("save");
+    let cepValue = '';
+    let data = null;
+
+    let armazem = [];
     const obterRegiao = (uf) => {
         const regioes = {
             "AC": "Norte", "AL": "Nordeste", "AP": "Norte", "AM": "Norte", "BA": "Nordeste", "CE": "Nordeste",
@@ -22,13 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     cep.addEventListener("input", () => {
-        let cepValue = cep.value.replace(/\D/g, '');
+        cepValue = cep.value.replace(/\D/g, '');
 
         if (cepValue.length === 8) {
             fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
+                    ''
 
                     if (data.erro) {
                         alert("CEP não encontrado");
@@ -49,5 +55,20 @@ document.addEventListener("DOMContentLoaded", () => {
                     alert('Erro ao consultar o CEP');
                 });
         }
+    });
+
+    nome.addEventListener("input", () => {
+        salvar.addEventListener("click", () => {
+            armazem.append(cepValue + "\n" +
+                (data.logradouro || "Logradouro não encontrado") + "\n" +
+                (data.complemento || "Complemento não encontrado") + "\n" +
+                (data.bairro || "Bairro não encontrado") + "\n" +
+                (data.localidade || "Localidade não encontrada") + "\n" +
+                (data.uf || "UF não encontrada") + "\n" +
+                obterRegiao(data.uf) + "\n" +
+                obterDDD(data.uf) + "\n");
+            console.log(armazem);
+
+        })
     });
 });
